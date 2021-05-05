2895d8a945c1482d5fbccc3a51ae4f9de60aa330
Remo <remo@kali.com>
2021-05-05 15:13:20 -0400

    Second Commit

---
+++ src/components/queries.tsx
@@ -0,0 +1,43 @@
+import gql from 'graphql-tag';
+
+export const QUERY_LAUNCH = gql`
+    query Launches {
+        launches {
+            flight_number
+            mission_name
+            launch_success
+            launch_year
+            links {
+                flickr_images
+                mission_patch_small
+            }
+            upcoming
+        }
+    }
+`;
+
+export const QUERY_LAUNCH_DETAILS = gql`
+    query LaunchInfo($id: String) {
+        launch(id: $id) {
+            mission_name
+            flight_number
+            details
+            launch_date_local
+            launch_site {
+                site_name
+            }
+            launch_success
+            upcoming
+            launch_year
+            links {
+                flickr_images
+                mission_patch_small
+                video_link
+            }
+            rocket {
+                rocket_name
+            }
+            ships
+        }
+    }
+`;
